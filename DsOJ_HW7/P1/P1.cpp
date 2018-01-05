f026084ed21b1b55147d754a9aea8488df06c4e0
SHun Feng <shunf4@live.cn>
2018-01-05 08:38:03 +0800

    create

---
+++ DsOJ_HW7/P1/P1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+template <typename VertexElem, typename EdgeWeight>
+class Graph
+{
+
+};
+
+template 
+
+#define FS_INFILE
+#define FS_OUTFILE
+int main()
+{
+#if(defined(_FS_DEBUG) && defined(FS_INFILE))
+	std::ifstream f0;
+	f0.open("p1.txt", std::ios::in);
+	auto cinbackup = std::cin.rdbuf();
+	std::cin.set_rdbuf(f0.rdbuf());
+#endif
+
+#if(defined(_FS_DEBUG) && defined(FS_OUTFILE))
+
+	std::ofstream f1;
+	f1.open("out.txt", std::ios::out);
+	auto coutbackup = std::cout.rdbuf();
+	std::cout.set_rdbuf(f1.rdbuf());
+#endif
+
+#if(defined(_FS_DEBUG) && defined(FS_INFILE))
+	f0.close();
+#endif
+
+#if(defined(_FS_DEBUG) && defined(FS_OUTFILE))
+	f1.close();
+#endif
+	return 0;
+}
